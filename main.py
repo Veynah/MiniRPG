a91cc8cfaabefd80964b829a7b05503b79e3ebc1
AdamTangiev <tangiev96@gmail.com>
2023-04-11 03:17:46 +0200

    ajout d'exit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit #importe le module exit de sys pour pouvoir fermer le programme sans créer d'erreur
 
 pygame.init()
 #On va faire notre interface de jeu
@@ -7,6 +8,7 @@ screen = pygame.display.set_mode((800,400))
 while True:#dans cette boucle on va update le jeu
     for event in pygame.event.get(): #une boucle qui va check pour n'importe quel input des joueurs
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pygame.quit() #pygame.quit est l'opposé de pygame.init
+            exit()
     pygame.display.update()
     
