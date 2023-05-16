6bebec574b9b09c6fa2f652e72ad2614ac5bfe37
AdamTangiev <tangiev96@gmail.com>
2023-05-16 15:57:06 +0200

    Input handler, changement main.py et player_class fait par Vlad va dans develop
    On met tout dans cette branche

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from pygame.locals import *
 from sys import exit #importe le module exit de sys pour pouvoir fermer le programme sans créer d'erreur
+from input_handler import InputHandler
+from player_class import Player
 
-pygame.init()
-#On va faire notre interface de jeu
-screen = pygame.display.set_mode((1920,1080))
-pygame.display.set_caption('MiniRPG') #Titre du jeu
-clock = pygame.time.Clock() #clock pour limiter le framerate
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption('MiniRPG')
+        self.clock = pygame.time.Clock()
+        self.input_handler = InputHandler()
+        self.player = Player("Player1")  # Added a name to the player
 
-while True:#dans cette boucle on va update le jeu
-    for event in pygame.event.get(): #une boucle qui va check pour n'importe quel input des joueurs
-        if event.type == pygame.QUIT:
-            pygame.quit() #pygame.quit est l'opposé de pygame.init
-            exit()
-    pygame.display.update()
-    clock.tick(60) #max fps 60
-    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.KEYDOWN:
+                    self.input_handler.handle_input(event)
+
+            action = self.input_handler.get_current_action()
+            if action:
+                action_method = getattr(self.player, action, None)
+                if action_method:
+                    action_method()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+if __name__ == "__main__":
+    Game().run()
