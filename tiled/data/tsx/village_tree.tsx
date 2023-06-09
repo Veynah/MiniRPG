085ce5ee8755e137b207d7d407236688b4f5f590
AdamTangiev <tangiev96@gmail.com>
2023-06-09 17:24:29 +0200

    change le chemin des assets pour que chacun puisse avoir accès aux tiled nécessaires

--- tiled/data/tsx/village_tree.tsx
+++ tiled/data/tsx/village_tree.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="village_tree" tilewidth="16" tileheight="16" tilecount="1050" columns="35">
- <image source="../../../../../3D Objects/Tiles/Legacy Fantasy - Kingdom Fortress - Update/Legacy Fantasy - Kingdom Fortress - Update/Tiles/Vegetation-export.png" width="560" height="480"/>
+ <image source="../../assets/Legacy Fantasy - Kingdom Fortress - Update/Legacy Fantasy - Kingdom Fortress - Update/Tiles/Vegetation-export.png" width="560" height="480"/>
 </tileset>
