a6821d9173b2e7dd4145edeac345079b5f0070ab
AdamTangiev <tangiev96@gmail.com>
2023-06-11 14:41:55 +0200

    Enlève les choses inutiles pour le moment

--- editor.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from settings import *
-
-class Editor:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
-        
-    def run(self, dt):
-        self.display_surface.fill('White')
