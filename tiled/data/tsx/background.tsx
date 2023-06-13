1f9e4ca1ea3ff8d12120b92639f0c6ca518ce577
AdamTangiev <tangiev96@gmail.com>
2023-06-13 19:16:20 +0200

    gestion plus simple du background

---
+++ tiled/data/tsx/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="background" tilewidth="16" tileheight="16" tilecount="420" columns="20">
+ <image source="../../assets/Legacy Fantasy - Kingdom Fortress - Update/Legacy Fantasy - Kingdom Fortress - Update/Background/village_background.png" width="335" height="336"/>
+</tileset>
